bd784a8604ea88d408408ca9ca9a02d1cc99c665
Seth Samuel <seth@visere.com>
2011-06-29 12:25:32 -0700

    Fixed bias in randomness

--- js/main2.js
+++ js/main2.js
@@ -177,12 +177,9 @@ function animate() {
 	for(var i=0;i<systems.length;i++){
 		if(systems[i].firework.get("exploded")){
 			for(var j=0;j<systems[i].geometry.vertices.length;j++){
-				systems[i].geometry.vertices[j].position.x += (systems[i].geometry.vertices[j].velocity.x+Math.random()*systems[i].firework.get("velocity"));
-				if(systems[i].geometry.vertices[j].velocity.x>0){
-					asdf=1;
-				}
-				systems[i].geometry.vertices[j].position.y += (systems[i].geometry.vertices[j].velocity.y+Math.random()*systems[i].firework.get("velocity")); 
-				systems[i].geometry.vertices[j].position.z += (systems[i].geometry.vertices[j].velocity.z+Math.random()*systems[i].firework.get("velocity")); 
+				systems[i].geometry.vertices[j].position.x += (systems[i].geometry.vertices[j].velocity.x+(Math.random()-0.5)*systems[i].firework.get("velocity"));
+				systems[i].geometry.vertices[j].position.y += (systems[i].geometry.vertices[j].velocity.y+(Math.random()-0.5)*systems[i].firework.get("velocity")); 
+				systems[i].geometry.vertices[j].position.z += (systems[i].geometry.vertices[j].velocity.z+(Math.random()-0.5)*systems[i].firework.get("velocity")); 
 				systems[i].geometry.vertices[j].velocity.y -= 0.01; 
 			}
 		}else{
